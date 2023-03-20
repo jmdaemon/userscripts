67acf48456ebd0cfb70e4fc1687b85eb13445be5
Joseph Diza <josephm.diza@gmail.com>
2023-03-19 19:52:43 -0700

    Fix userscript not folding filenames properly

--- desuarchive_expand_filenames/desuarchive_expand_filenames.js
+++ desuarchive_expand_filenames/desuarchive_expand_filenames.js
@@ -1,24 +1,8 @@
-var post_image_filenames = document.querySelectorAll('.post_file_filename');
-
-/*
-Needs to be lazy expanded
-function expand_filename(post) {
-  var title = post.title;
-  var folded_title = post.childNodes.innerHTML;
-  post.innerHTML = title;
-}
-*/
-
-function get_element_text(element) {
-  var text = element.innerText || element.textContent;
-  return text;
-}
-
 //const expand_filename = (post) => {
 const expand_filename = (event) => {
   console.log('[expand_filename]');
 
-  var post = event.currentTarget.post;
+  var post = event.currentTarget;
   console.log(event.currentTarget);
 
   console.log(post);
@@ -33,10 +17,10 @@ const expand_filename = (event) => {
   //var folded = post.childNodes.text;
   //post.folded = folded;
 
-  var folded = post.childNodes.data;
-  post.folded = folded;
+  //var folded = post.childNodes.data;
+  //post.folded = folded;
 
-  console.log('Folded: ' + folded);
+  //console.log('Folded: ' + folded);
 
   post.innerHTML = title;
 }
@@ -45,25 +29,32 @@ const fold_filename = (event) => {
   console.log('[fold_filename]');
   console.log(event);
 
-  var target = event.target;
-  console.log(target);
-
-  var post = target.post;
+  var post = event.target;
   console.log(post);
 
   var folded = post.folded;
   console.log(folded);
 
-  target.innerHTML = folded;
+  post.innerHTML = folded;
 }
 
-for (index in post_image_filenames) {
-  var post = post_image_filenames[index];
-  
-  console.log(post);
-	console.log(post.childNodes);
-  
-  post.addEventListener("mouseover", expand_filename);
-  post.addEventListener("mouseout", fold_filename);
-  post.post = post;
+function init() { 
+  var post_image_filenames = document.querySelectorAll('.post_file_filename');
+  console.log('[init]');
+  for (index in post_image_filenames) {
+    var post = post_image_filenames[index];
+    
+    // Debug messages
+    console.log(post);
+    console.log(post.childNodes);
+    
+    post.addEventListener("mouseover", expand_filename);
+    post.addEventListener("mouseout", fold_filename);
+
+    // Save the current folded text
+    var folded = post.innerHTML;
+    console.log(folded);
+    post.folded = folded;
+  }
 }
+init();
