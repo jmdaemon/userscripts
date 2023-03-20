8414b1c7d7f0e844a2b40d08853bb9e69a1dc1e2
Joseph Diza <josephm.diza@gmail.com>
2023-03-19 19:41:17 -0700

    Add non functional folding of filenames

--- desuarchive_expand_filenames/desuarchive_expand_filenames.js
+++ desuarchive_expand_filenames/desuarchive_expand_filenames.js
@@ -9,12 +9,52 @@ function expand_filename(post) {
 }
 */
 
+function get_element_text(element) {
+  var text = element.innerText || element.textContent;
+  return text;
+}
+
 //const expand_filename = (post) => {
 const expand_filename = (event) => {
+  console.log('[expand_filename]');
+
   var post = event.currentTarget.post;
+  console.log(event.currentTarget);
+
+  console.log(post);
   var title = post.title;
-  var folded_title = post.childNodes.innerHTML;
+
+  //var folded = post.childNodes.innerHTML;
+  //post.folded = folded;
+
+  //var folded = get_element_text(post.childNodes);
+  //post.folded = folded;
+
+  //var folded = post.childNodes.text;
+  //post.folded = folded;
+
+  var folded = post.childNodes.data;
+  post.folded = folded;
+
+  console.log('Folded: ' + folded);
+
   post.innerHTML = title;
+}
+
+const fold_filename = (event) => {
+  console.log('[fold_filename]');
+  console.log(event);
+
+  var target = event.target;
+  console.log(target);
+
+  var post = target.post;
+  console.log(post);
+
+  var folded = post.folded;
+  console.log(folded);
+
+  target.innerHTML = folded;
 }
 
 for (index in post_image_filenames) {
@@ -23,7 +63,7 @@ for (index in post_image_filenames) {
   console.log(post);
 	console.log(post.childNodes);
   
-	//post.addEventListener("mouseover", expand_filename(post));
   post.addEventListener("mouseover", expand_filename);
+  post.addEventListener("mouseout", fold_filename);
   post.post = post;
 }
