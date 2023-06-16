29c7bae52c67ea4af0d57f5914285edcabc988d4
Joseph Diza <josephm.diza@gmail.com>
2023-06-16 16:27:19 -0700

    Upload userscript builds

---
+++ desuarchive_expand_filenames/dist/desuarchive-expand-filenames.user.js
@@ -0,0 +1,58 @@
+
+// ==UserScript==
+// @name        desuarchive-expand-filenames
+// @namespace   Desuarchive Expand Filenames
+// @description Expand post filenames on mouse hover
+// @match       https://desuarchive.org/*
+// @version     0.1.0
+// @author      Joseph Diza
+// @require     https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@2,npm/@violentmonkey/ui@0.7
+// @grant       GM_addStyle
+// ==/UserScript==
+
+(function () {
+'use strict';
+
+var src = {};
+
+var expand_filename = function expand_filename(event) {
+  console.log('[expand_filename]');
+  var post = event.currentTarget;
+  console.log(event.currentTarget);
+  console.log(post);
+  var title = post.title;
+  post.innerHTML = title;
+};
+var fold_filename = function fold_filename(event) {
+  console.log('[fold_filename]');
+  console.log(event);
+  var post = event.target;
+  console.log(post);
+  var folded = post.folded;
+  console.log(folded);
+  post.innerHTML = folded;
+};
+function init() {
+  var post_image_filenames = document.querySelectorAll('.post_file_filename');
+  console.log('[init]');
+  var index = 0;
+  for (index in post_image_filenames) {
+    var post = post_image_filenames[index];
+
+    // Debug messages
+    console.log(post);
+    console.log(post.childNodes);
+    post.addEventListener("mouseover", expand_filename);
+    post.addEventListener("mouseout", fold_filename);
+
+    // Save the current folded text
+    var folded = post.innerHTML;
+    console.log(folded);
+    post.folded = folded;
+  }
+}
+init();
+
+return src;
+
+})();
