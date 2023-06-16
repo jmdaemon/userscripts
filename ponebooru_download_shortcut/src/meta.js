ad94f43596e1b5f5aa6e5e9042cf18ee354d2deb
Joseph Diza <josephm.diza@gmail.com>
2023-06-16 16:33:13 -0700

    Auto update userscript

--- ponebooru_download_shortcut/src/meta.js
+++ ponebooru_download_shortcut/src/meta.js
@@ -6,6 +6,8 @@
 // @include     *://twibooru.org/*
 // @include     *://ponybooru.org/*
 // @include     *://derpibooru.org/*
+// @updateURL   https://github.com/jmdaemon/userscripts/raw/master/ponebooru_download_shortcut/dist/ponebooru_download_shortcut.user.js
+// @downloadURL https://github.com/jmdaemon/userscripts/raw/master/ponebooru_download_shortcut/dist/ponebooru_download_shortcut.user.js
 // @grant       GM_addStyle
 // @version     process.env.VERSION
 // @author      process.env.AUTHOR
