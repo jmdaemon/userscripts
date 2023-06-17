93b65cd7cfb3e21432ce062043d493a5abff97a2
Joseph Diza <josephm.diza@gmail.com>
2023-06-16 17:15:05 -0700

    Clean up download shortcut script

--- ponebooru_download_shortcut/src/ponebooru_download_shortcut.js
+++ ponebooru_download_shortcut/src/ponebooru_download_shortcut.js
@@ -1,29 +1,26 @@
-
 // Keep track of clicked keys
 var isKeyPressed = {
-    'd': false, // ASCII code for 'b'
+    'd': false, // ASCII code for 'd'
 };
 
-document.onkeydown = (keyDownEvent) => {
- //Prevent default key actions, if desired
- keyDownEvent.preventDefault();
+const DOWNLOAD_BTN_SELECTOR = 'a[title="Download (tags in filename)"]';
+
+function download_via_shortcut(e) {
+  // Disable browser shortcuts
+  e.preventDefault();
 
   // Track down key click
- isKeyPressed[keyDownEvent.key] = true;
+  isKeyPressed[e.key] = true;
 
-  if (isKeyPressed['d'] && keyDownEvent.ctrlKey) {
-    var download_btn = document.querySelectorAll('a[title="Download (tags in filename)"]')[0];
+  if (isKeyPressed['d'] && e.ctrlKey) {
+    var download_btn = document.querySelectorAll(DOWNLOAD_BTN_SELECTOR)[0];
     download_btn.click();
   }
+}
+
+document.onkeydown = download_via_shortcut;
+
+// Reset event handling
+document.onkeyup = (e) => {
+  isKeyPressed[e.key] = false;
 };
-
-document.onkeyup = (keyUpEvent) => {
-
- // Prevent default key actions, if desired
- keyUpEvent.preventDefault();
-
- // Track down key release
- isKeyPressed[keyDownEvent.key] = false;
-};
-
-//document.querySelectorAll('a[title="Download (tags in filename)"]');
