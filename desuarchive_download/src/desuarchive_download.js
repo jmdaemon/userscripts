a78897659a81b79d7021b330f8dc7e7c18624f6f
Joseph Diza <josephm.diza@gmail.com>
2023-07-08 19:44:31 -0700

    Remove dead code

--- desuarchive_download/src/desuarchive_download.js
+++ desuarchive_download/src/desuarchive_download.js
@@ -1,18 +1,3 @@
-//function downloadFile(url, fileName) {
-
-  //fetch(url, { method: 'get', mode: 'no-cors', referrerPolicy: 'no-referrer' })
-    //.then(res => res.blob())
-    //.then(res => {
-      //const aElement = document.createElement('a');
-      //aElement.setAttribute('download', fileName);
-      //const href = URL.createObjectURL(res);
-      //aElement.href = href;
-      //aElement.setAttribute('target', '_blank');
-      //aElement.click();
-      //URL.revokeObjectURL(href);
-    //});
-//};
-
 function downloadWithFilename(event) {
   let btn_download = event.currentTarget;
   let postToolbar = btn_download.parentElement;
@@ -21,23 +6,25 @@ function downloadWithFilename(event) {
   let source = imageElement.href;
   let filename = imageElement.download;
   
-  //downloadFile(source, filename);
   GM_download(source, filename);
 }
 
-const css_class_post_toolbar = '.post_file_controls';
-const toolbar_file_posts = document.querySelectorAll(css_class_post_toolbar);
+function main() {
+  const css_class_post_toolbar = '.post_file_controls';
+  const toolbar_file_posts = document.querySelectorAll(css_class_post_toolbar);
 
-for (let postIndex = 0; postIndex < toolbar_file_posts.length; postIndex++) {
-  // Create the button
-  let btn_download = document.createElement('a');
-  btn_download.classList.add('btnr', 'parent');
-  btn_download.textContent = 'Download';
-  
-  // Register the callback
-  btn_download.addEventListener('click', downloadWithFilename, true);
+  for (let postIndex = 0; postIndex < toolbar_file_posts.length; postIndex++) {
+    // Create the button
+    let btn_download = document.createElement('a');
+    btn_download.classList.add('btnr', 'parent');
+    btn_download.textContent = 'Download';
+    
+    // Register the callback
+    btn_download.addEventListener('click', downloadWithFilename, true);
 
-  // Add the btn to every post
-  let postToolbar = toolbar_file_posts[postIndex];
-  postToolbar.append(btn_download);
+    // Add the btn to every post
+    let postToolbar = toolbar_file_posts[postIndex];
+    postToolbar.append(btn_download);
+  }
 }
+main();
